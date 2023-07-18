cc798bfb8bccc65b10f3e1118d38322733ec94ae
Floris <floris.defeyter@kuleuven.be>
2023-07-18 18:50:30 +0200

    Add test for score matrix

---
+++ src/utils/inference/tests/test_score_matrix.py
@@ -0,0 +1,48 @@
+import torch
+
+from src.utils.inference import get_score_matrix
+
+
+def test_get_score_matrix():
+    dl_gal = [
+        (torch.tensor([[1.0,  0.0]]), torch.tensor([0])),
+        (torch.tensor([[-1.0, 0.0]]), torch.tensor([1])),
+    ]
+    dl_quer = [
+        (torch.tensor([[1.0,  0.0]]), torch.tensor([0])),
+        (torch.tensor([[0.0,  1.0]]), torch.tensor([0])),
+        (torch.tensor([[-1.0, 0.0]]), torch.tensor([1])),
+        (torch.tensor([[0.0, -1.0]]), torch.tensor([1])),
+    ]
+
+    exp_scores = torch.tensor([
+        [1.0, -1.0],
+        [0.0,  0.0],
+        [-1.0, 1.0],
+        [0.0,  0.0],
+    ])
+    exp_q_labels = torch.tensor([
+        0,
+        0,
+        1,
+        1
+    ])
+    exp_g_labels = torch.tensor([0, 1])
+
+    ret_scores, ret_q_labels, ret_g_labels = get_score_matrix(
+        MockModel(),
+        device='cpu',
+        dl_gal=dl_gal,
+        dl_quer=dl_quer
+    )
+
+    assert (exp_scores == ret_scores).all()
+    assert (exp_q_labels == ret_q_labels).all()
+    assert (exp_g_labels == ret_g_labels).all()
+
+
+class MockModel:
+    training = False
+
+    def __call__(self, x):
+        return x
