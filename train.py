9c5cb2762a2217dff8ab3cf0401daa283a9df04f
Floris <floris.defeyter@kuleuven.be>
2023-07-20 10:11:39 +0200

    Merge train.py and training_loop.py

--- train.py
+++ train.py
@@ -1,23 +1,26 @@
 from argparse import ArgumentParser
-import logging
 from pathlib import Path
-from typing import Optional, List, Tuple
+import sys
+from typing import Callable, Optional, List, Tuple
 
 import torch
 from torch import nn
-from torch.optim import SGD
-from torch.optim.lr_scheduler import LinearLR
+from torch.optim import Optimizer, SGD
+from torch.optim.lr_scheduler import LRScheduler, LinearLR
 from torch.utils.data import DataLoader
 from torchvision.models._api import Weights
 from torchvision import models
+from tqdm import tqdm
 import wandb
 
 from src.model import Recognizer
 from src.data import RecogDataset, TRAIN_SUBSET, QUERY_SUBSET,\
     GALLERY_SUBSET, get_data_transforms
-from src.utils.inference import collate_with_three_crops,\
-    get_embeddings_three_crops
-from src.training_loop import TrainingLoop
+from src.metrics import accuracy, pr_metrics, hard_pos_neg_scores
+from src.utils.inference import get_score_matrix, avg_ref_embs,\
+    collate_with_three_crops, get_embeddings_three_crops
+from src.utils.training import create_checkpoints, log, RunningExtrema,\
+    MAX, MIN
 
 
 def run_training(
@@ -164,33 +167,176 @@ def run_training(
         total_iters=lr_warmup_steps
     )
 
-    # Start training
-    training_loop = TrainingLoop(
-        model=model,
-        loss_fn=loss_fn,
-        optimizer=optimizer,
-        lr_scheduler=lr_scheduler,
-        device=device,
-        num_epochs=num_epochs,
-        dl_train=dl_train,
-        dl_val_gal=dl_val_gal,
-        dl_val_quer=dl_val_quer,
-        save_last=save_last,
-        save_best=save_best,
-        best_metric=best_metric,
-        is_higher_better=is_higher_better,
-        ckpts_path=ckpts_path,
-        run_name=run_name,
-        get_embeddings_fn=get_embeddings_three_crops if use_three_crop
-        else None
+    # Initialize Running Extrema
+    running_extrema_best = RunningExtrema(
+        MAX if is_higher_better
+        else MIN
     )
-    try:
-        training_loop.run()
-    except KeyboardInterrupt:
-        logging.info("Training interrupted. Returning current model...")
-        pass
+    running_extrema_worst = RunningExtrema(
+        MIN if is_higher_better
+        else MAX
+    )
+    ckpt_dir = Path(ckpts_path)
 
-    return model
+    running_extrema_best.clear()
+    running_extrema_worst.clear()
+
+    # Training loop
+    for epoch_idx in tqdm(range(num_epochs), leave=True):
+        # Training epoch
+        model.train()
+        training_epoch(
+            model=model,
+            loss_fn=loss_fn,
+            optimizer=optimizer,
+            lr_scheduler=lr_scheduler,
+            epoch_idx=epoch_idx,
+            device=device,
+            dl_train=dl_train,
+        )
+
+        # Validation epoch
+        model.eval()
+        validation_epoch(
+            model=model,
+            epoch_idx=epoch_idx,
+            device=device,
+            dl_val_gal=dl_val_gal,
+            dl_val_quer=dl_val_quer,
+            running_extrema_best=running_extrema_best,
+            running_extrema_worst=running_extrema_worst,
+            save_last=save_last,
+            save_best=save_best,
+            best_metric=best_metric,
+            ckpt_dir=ckpt_dir,
+            run_name=run_name,
+            get_embeddings_fn=get_embeddings_three_crops if use_three_crop
+            else None,
+        )
+
+
+train_batch_idx = -1
+
+
+def training_epoch(
+    model: nn.Module,
+    loss_fn: nn.Module,
+    optimizer: Optimizer,
+    lr_scheduler: LRScheduler,
+    epoch_idx: int,
+    device: torch.device,
+    dl_train: DataLoader,
+):
+    global train_batch_idx
+    for (train_batch_idx, batch) in enumerate(
+        tqdm(dl_train, leave=False), start=train_batch_idx + 1
+    ):
+        imgs, targets = batch
+        imgs = imgs.to(device)
+        targets = targets.to(device)
+
+        # Compute loss
+        preds = model(imgs)
+        loss = loss_fn(preds, targets)
+
+        # Take optimization + LR scheduler step
+        loss.backward()
+        optimizer.step()
+        lr_scheduler.step()
+        model.zero_grad()
+
+        # Log results and current LR
+        log_dict = {
+            loss_fn.__class__.__name__: loss,
+        }
+        log(log_dict, epoch_idx=epoch_idx,
+            batch_idx=train_batch_idx,
+            section='Train')
+        log(
+            dict(LR=lr_scheduler.get_last_lr()[0]),
+            epoch_idx=epoch_idx,
+            batch_idx=train_batch_idx
+        )
+
+    if torch.isnan(loss):
+        sys.exit('Loss is NaN. Exiting...')
+
+
+def validation_epoch(
+    model: nn.Module,
+    epoch_idx: int,
+    device: torch.device,
+    dl_val_gal: DataLoader,
+    dl_val_quer: DataLoader,
+    running_extrema_best: RunningExtrema,
+    running_extrema_worst: RunningExtrema,
+    save_last: bool,
+    save_best: bool,
+    best_metric: str,
+    ckpt_dir: Path,
+    run_name: str,
+    get_embeddings_fn: Optional[Callable] = None
+):
+    # Compute score matrix and corresponding labels
+    scores, quer_labels, gal_labels = get_score_matrix(
+        model,
+        device,
+        dl_val_gal,
+        dl_val_quer,
+        get_embeddings_fn=get_embeddings_fn,
+    )
+    # Compute score matrix and corresponding labels when using average
+    # reference embeddings in the gallery
+    scores_avg_refs, _, gal_labels_avg_refs = get_score_matrix(
+        model,
+        device,
+        dl_val_gal,
+        dl_val_quer,
+        get_embeddings_fn=get_embeddings_fn,
+        agg_gal_fn=avg_ref_embs
+    )
+
+    # Compute PR metrics (only for non-aggregated refs)
+    val_log_dict = pr_metrics(scores, quer_labels, gal_labels)
+
+    # Compute top-1 accuracy
+    val_log_dict.update({
+        'Accuracy': accuracy(scores, quer_labels, gal_labels)
+    })
+    val_log_dict.update({
+        'Accuracy (avg refs)': accuracy(scores_avg_refs, quer_labels,
+                                        gal_labels_avg_refs)
+    })
+
+    # Compute distribution of hard positive and negative similarities
+    val_log_dict.update(
+        hard_pos_neg_scores(scores, quer_labels, gal_labels)
+    )
+    val_log_dict.update({
+        f'{k} (avg refs)': v
+        for k, v in hard_pos_neg_scores(scores_avg_refs, quer_labels,
+                                        gal_labels_avg_refs).items()
+    })
+
+    # Log validation metrics
+    log(val_log_dict, epoch_idx=epoch_idx, section='Val')
+
+    # Check if the value of the metric to optimize is the best
+    best_metric_val = val_log_dict[best_metric]
+    is_best = running_extrema_best.is_new_extremum(best_metric,
+                                                   best_metric_val)
+    # Create checkpoints
+    create_checkpoints(model, run_name, ckpt_dir, save_best=save_best and
+                       is_best, save_last=save_last)
+
+    # Update and log running extrema
+    running_extrema_best.update_dict(val_log_dict)
+    running_extrema_worst.update_dict(val_log_dict)
+
+    log(running_extrema_best.extrema_dict, epoch_idx=epoch_idx,
+        section='Val')
+    log(running_extrema_worst.extrema_dict, epoch_idx=epoch_idx,
+        section='Val')
 
 
 def str_list_arg_type(arg):
