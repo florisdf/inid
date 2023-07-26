918afab776c306373531f4659df7b75c9ece0dd0
Floris De Feyter <floris_defeyter@hotmail.com>
2023-07-26 14:28:14 +0200

    Remove obsolete files

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/florisdf/recognite',
-    version='0.1.0-rc0',
+    version='0.1.0-rc1',
     zip_safe=False,
 )
